76f87a283584b971eb9d43e617bef14ecabe9542
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2021-07-16 20:04:37 +0530

    Updated checkout screen by adding bloc to it

--- lib/pages/checkout.dart
+++ lib/pages/checkout.dart
@@ -9,14 +9,20 @@ class Checkout extends StatelessWidget {
         stream: bloc.getStream,
         initialData: bloc.allItems,
         builder: (context, snapshot) {
-          return snapshot.data['cart items'].length > 0
+          return snapshot.data['inactive'].length > 0
               ? Column(
                   children: <Widget>[
                     /// The [checkoutListBuilder] has to be fixed
-                    /// in an expanded widget to ensure it 
+                    /// in an expanded widget to ensure it
                     /// doesn't occupy the whole screen and leaves
                     /// room for the the RaisedButton
                     Expanded(child: checkoutListBuilder(snapshot)),
+                    RaisedButton(
+                      onPressed: () {
+                        bloc.gettingvar();
+                      },
+                      child: Text("Change var"),
+                    ),
                     RaisedButton(
                       onPressed: () {},
                       child: Text("Checkout"),
@@ -25,7 +31,17 @@ class Checkout extends StatelessWidget {
                     SizedBox(height: 40)
                   ],
                 )
-              : Center(child: Text("You haven't taken any item yet"));
+              : Column(
+                  children: [
+                    Center(child: Text("You haven't taken any item yet")),
+                    RaisedButton(
+                      onPressed: () {
+                        bloc.gettingvar();
+                      },
+                      child: Text("Change var"),
+                    ),
+                  ],
+                );
         },
       ),
     );
@@ -34,12 +50,12 @@ class Checkout extends StatelessWidget {
 
 Widget checkoutListBuilder(snapshot) {
   return ListView.builder(
-    itemCount: snapshot.data["cart items"].length,
+    itemCount: snapshot.data["inactive"].length,
     itemBuilder: (BuildContext context, i) {
-      final cartList = snapshot.data["cart items"];
+      final cartList = snapshot.data["inactive"];
       return ListTile(
-        title: Text(cartList[i]['name']),
-        subtitle: Text("\$${cartList[i]['price']}"),
+        title: Text(cartList[i]['value']),
+        // subtitle: Text("\$${cartList[i]['price']}"),
         trailing: IconButton(
           icon: Icon(Icons.remove_shopping_cart),
           onPressed: () {
